93cc0421b892abbcc902dc670190ef8ddf6016cd
8pathcreative <neilhumphreydesign@gmail.com>
2025-02-24 16:45:19 -0600

    dsds

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,28 +1,21 @@
 // src/providers/AppProvider.tsx
-import React, { createContext, useContext, useState } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
-const AppContext = createContext(null);
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ 
   children 
 }) => {
-  const [state, setState] = useState({});
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <AuthProvider>
-          <AppContext.Provider value={{ state, setState }}>
-            {children}
-          </AppContext.Provider>
+          {children}
         </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
 };
-
-export const useAppContext = () => useContext(AppContext);
