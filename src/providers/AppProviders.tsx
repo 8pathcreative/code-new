b1047b88cae9470a75240c3bf5c37eab9fad53b3
8path Studio <neilhumphreydesign@gmail.com>
2025-02-27 02:44:14 +0000

    h

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,7 +1,7 @@
 // src/providers/AppProvider.tsx
 import React, { createContext, useContext, useState } from 'react';
-import { ThemeProvider } from '@/contexts/ThemeContext';
-import { AuthProvider } from '@/contexts/AuthContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
+import { AuthProvider } from '../contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
